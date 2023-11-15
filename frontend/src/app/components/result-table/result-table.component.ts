a539224c977910e9b270f11e20238e76f49d1d7e
long <nguyenthanhlong@morenergy.net>
2023-11-15 21:50:07 +0100

    feat: expand all collapse all

--- frontend/src/app/components/result-table/result-table.component.ts
+++ frontend/src/app/components/result-table/result-table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, ViewChildren, QueryList } from '@angular/core';
+import { MatExpansionPanel } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-result-table',
@@ -8,11 +9,38 @@ import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/cor
 export class ResultTableComponent implements OnChanges{
   @Input() results: any[]
 
+  @ViewChildren(MatExpansionPanel) panels: QueryList<MatExpansionPanel>
+
   isArray: boolean
+
+  isPanelExpanded: boolean
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.results && changes.results.currentValue) {
+
+      /** Check if it's array or not, because when user only click
+       * to show detail, there would be only one object instead of 
+       * array of objects. That means we have different logic for 
+       * displaying them.
+       * 
+       * This condition will decide which display logic will we use
+       * in the template.
+       */
       this.isArray = Array.isArray(this.results)
+    }
+  }
+
+  expandCollapseAllPanels() {
+    this.isPanelExpanded = !this.isPanelExpanded
+
+    if (this.isPanelExpanded) {
+      this.panels.forEach(panel => {
+        panel.open()
+      })
+    } else {
+      this.panels.forEach(panel => {
+        panel.close()
+      })
     }
   }
 
