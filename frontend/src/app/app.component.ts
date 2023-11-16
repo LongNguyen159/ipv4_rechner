06ce1d36b5e10487a778f2270f0e567535c38a2a
long <nguyenthanhlong@morenergy.net>
2023-11-16 10:12:12 +0100

    feat: navigate to compute on init

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ipv4_rechner';
+
+
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.router.navigate(['/compute'])
+  }
 }
